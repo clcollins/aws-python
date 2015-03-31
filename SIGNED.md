1bda7daa20f10b4eec27c0d9cbb388e7eab12b18
Chris Collins <collins.christopher@gmail.com>
2015-03-31 16:24:09 -0400

    adding baseimage patching script

--- SIGNED.md
+++ SIGNED.md
@@ -3,9 +3,9 @@
 -----BEGIN PGP SIGNATURE-----
 Version: GnuPG v1
 
-iEYEABECAAYFAlUbAeYACgkQte6EFif3vze9BwCgyceOyUe/oD+CLSK1PewPhxGr
-06UAoKFC3nakKRKMexFP9ckuUhjQ33mb
-=/9HT
+iEYEABECAAYFAlUbAlcACgkQte6EFif3vzdjWgCg3vQ/bKymoMaXQwzAvCOF0mFv
+QJsAmwQiZ095SLlVjaBZovDd6RkkXNX6
+=FqWx
 -----END PGP SIGNATURE-----
 
 ```
@@ -17,13 +17,14 @@ iEYEABECAAYFAlUbAeYACgkQte6EFif3vze9BwCgyceOyUe/oD+CLSK1PewPhxGr
 #### Expect
 
 ```
-size   exec  file                 contents                                                        
-             ./                                                                                   
-14             .gitignore         5a1c48902aa4e38786229ed6c871d85273e6680846fbf4a65ae5710981145d9f
-35122          LICENSE            12ac5047f2af0522f06798b1589ffc4599bc29c91f954d7874e0320634e777c0
-55             README.md          36d73bc04c3ea1864f8e244aa6ad0aa07ff62b3b2162b9c0ac9145ed252980c5
-349            list_images.py     f8908463ff42a031199ee4c16f6125b43d4005fc6c049c82b6a1fdeb683d6eb8
-423            list_instances.py  4df8fc57987c504f6e6facf14f6906fbe88442a4ccdcf94776b52d33f6bcdf50
+size   exec  file                  contents                                                        
+             ./                                                                                    
+14             .gitignore          5a1c48902aa4e38786229ed6c871d85273e6680846fbf4a65ae5710981145d9f
+35122          LICENSE             12ac5047f2af0522f06798b1589ffc4599bc29c91f954d7874e0320634e777c0
+55             README.md           36d73bc04c3ea1864f8e244aa6ad0aa07ff62b3b2162b9c0ac9145ed252980c5
+349            list_images.py      f8908463ff42a031199ee4c16f6125b43d4005fc6c049c82b6a1fdeb683d6eb8
+423            list_instances.py   4df8fc57987c504f6e6facf14f6906fbe88442a4ccdcf94776b52d33f6bcdf50
+3769           patch_baseimage.py  dbdad75cc81cc139fe8b305f96cae73fc72c81fa0b51e9956bd008a9db01cc8b
 ```
 
 #### Ignore
---
+++ patch_baseimage.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+import boto.ec2
+import time
+
+# Assumes a three version base AMI image maintenace program
+
+def_region = 'us-west-2'
+def_type = 't2.micro'
+
+# The default Baseimage AMI names
+ami = 'baseimage'
+ami_prev = "baseimage_previous"
+ami_last = "baseimage_last"
+ami_new = "PENDING"
+
+key = 'root_aws'
+user_data = ''
+dry_run = False
+verbose = True
+
+
+# Add any extra security groups to the default list
+def_sec_grp = ['INTERNAL SSH']
+add_sec_grps = ['INTERNAL Pings']
+
+sec_grps = def_sec_grp + add_sec_grps
+
+conn = boto.ec2.connect_to_region(def_region)
+
+
+def debug(string):
+    if verbose:
+        print string
+
+
+def warn(string):
+    print string
+
+
+def get_image_id(name):
+    debug(name)
+    images = conn.get_all_images(owners='self')
+
+    for image in images:
+        if image.name == name:
+            debug("name:" + name + ", ID:" + image.id)
+            return image.id
+
+
+def rotate_ami(new_id):
+    ami_id = get_image_id(ami)
+    previous_id = get_image_id(ami_prev)
+    last_id = get_image_id(ami_last)
+
+    if (ami_id) and (not dry_run):
+
+        debug("Rotating images")
+        # If we have a baseimage,
+        # then check to see if we have a previous image
+        if previous_id:
+            # If we have a previous image,
+            # then check to see if we have a last image
+            if last_id:
+                debug("Rotating: " + last_id)
+                conn.deregister_image(last_id, dry_run=dry_run)
+
+            debug("Rotating: " + previous_id)
+            conn.copy_image(def_region, previous_id, name=ami_last)
+            conn.deregister_image(previous_id, dry_run=dry_run)
+
+        debug("Rotating: " + ami_id)
+        conn.copy_image(def_region, ami_id, name=ami_prev)
+        conn.deregister_image(ami_id, dry_run=dry_run)
+
+        debug("Renaming: " + new_id)
+        conn.copy_image(def_region, new_id, name=ami)
+        conn.deregister_image(new_id, dry_run=dry_run)
+
+    else:
+
+        warn("Rotation of images did not occur: Dry Run flag set")
+
+
+def create_new_ami(instance):
+    warn("BASEIMAGE MUST BECOME AVAILABLE - THIS MAY TAKE A WHILE")
+    create_image(instance, name=ami_new, description=ami_new, dry_run=dry_run)
+
+    new = get_image_id(ami_new)
+
+    status = new.update()
+    while status == 'pending':
+        time.sleep(10)
+        status = new.update()
+
+    if status == 'available':
+        # The new base image has been created and is available
+        # begin the rotation
+        rotate_ami(new)
+
+    else:
+        warn('Status: ' + status)
+        return None
+
+
+def launch_instance(region, itype, ami, key, user_data, sec_grps, dry_run):
+
+    reservation = conn.run_instances(image_id=ami,
+                                     instance_type=itype,
+                                     key_name=key,
+                                     user_data=user_data,
+                                     security_groups=sec_grps,
+                                     dry_run=dry_run)
+
+    i = reservation.instances[0]
+    status = i.update()
+    while status == 'pending':
+        time.sleep(10)
+        status = i.update()
+
+    if status == 'running':
+
+            i.add_tag('Name', 'baseimage', dry_run=dry_run)
+            debug("Instance created: " + i.id)
+            return i.id
+
+    else:
+        warn('Status: ' + status)
+        return None
+
+
+# def ssh_and_patch(instance_id):
+
+    # Do something
+
+
+# THE ORDER:
+# ami_id = get_image_id(ami)
+# instance = launch_instance(def_region,
+#                            def_type,
+#                            ami_id,
+#                            key,
+#                            user_data,
+#                            sec_grps,
+#                            dry_run)
+
+# ssh_and_patch(instance) & shutdown!
+# create_new_ami(instance) - runs rotate when done
